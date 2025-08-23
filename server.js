b2f24708851de702e33edeca007e89dab643b030
Victoria <130781096+Vico26@users.noreply.github.com>
2025-08-23 15:51:08 -0600

    Updates del comentario
    Especifica que es lo que se esta trabajando y usando en el proyecto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /*BD-Proyecto programado 1
     Integrantes: Victoria Molina Martínez
                   Gabriel Perez Tullocks
-    
+Se esta utilizando  Node.js con Express para el servidor
+Para el servidor se esta manejando SQL Server, en conjunto con MSSQL
 */
